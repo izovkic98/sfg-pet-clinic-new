11c9f7411f0917e9ff02783cfe6d7e79f2d9fa26
izovkic98 <izovkic@tvz.hr>
2021-05-09 13:27:54 +0200

    Create Vet Index page and controller. Closes #6

---
+++ sfg-pet-clinic-web/src/main/java/com/izovkic/sfgpetclinicnew/controllers/VetController.java
@@ -0,0 +1,11 @@
+package com.izovkic.sfgpetclinicnew.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
