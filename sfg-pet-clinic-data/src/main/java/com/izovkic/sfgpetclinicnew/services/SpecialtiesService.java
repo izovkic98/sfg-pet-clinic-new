b4b9e844d7e70d3082eb29105db263505c2c89c1
izovkic98 <izovkic@tvz.hr>
2021-05-15 21:49:45 +0200

    Added Specialties Map Impl. Closes #31

---
+++ sfg-pet-clinic-data/src/main/java/com/izovkic/sfgpetclinicnew/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.izovkic.sfgpetclinicnew.services;
+
+import com.izovkic.sfgpetclinicnew.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+
+
+
+}
