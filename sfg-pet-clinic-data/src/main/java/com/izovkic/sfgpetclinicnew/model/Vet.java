bb9142b912b938f7a2ac3a577b4636efc6c900cb
izovkic98 <izovkic@tvz.hr>
2021-05-15 13:48:53 +0200

    added specialities, added to owner. Closes #27

--- sfg-pet-clinic-data/src/main/java/com/izovkic/sfgpetclinicnew/model/Vet.java
+++ sfg-pet-clinic-data/src/main/java/com/izovkic/sfgpetclinicnew/model/Vet.java
@@ -1,8 +1,16 @@
 package com.izovkic.sfgpetclinicnew.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
-    public Vet() {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
     }
 
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
