dc65155f9067197e286120b59a72505aa12c5a57
izovkic98 <izovkic@tvz.hr>
2021-07-23 19:19:34 +0200

    Rijesen Bug i dodane sitnice Closes #62

--- sfg-pet-clinic-data/src/main/java/com/izovkic/sfgpetclinicnew/services/map/OwnerServiceMap.java
+++ sfg-pet-clinic-data/src/main/java/com/izovkic/sfgpetclinicnew/services/map/OwnerServiceMap.java
@@ -8,6 +8,7 @@ import com.izovkic.sfgpetclinicnew.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -81,5 +82,12 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
                 .findFirst()
                 .orElse(null);
 
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+//        impl
+
+        return null;
     }
 }
--- sfg-pet-clinic-web/src/test/java/com/izovkic/sfgpetclinicnew/controllers/OwnerControllerTest.java
+++ sfg-pet-clinic-web/src/test/java/com/izovkic/sfgpetclinicnew/controllers/OwnerControllerTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -48,24 +49,30 @@ class OwnerControllerTest {
 
     }
 
-    @Test
-    void ownersList() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
-        mockMvc.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
-    }
+
 
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("notImplemented"));
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
 
         verifyNoMoreInteractions(ownerService);
     }
 
+    @Test
+    void processFindFormReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                        Owner.builder().id(2L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+
+    }
 
     @Test
     void displayOwner() throws Exception {
@@ -76,4 +83,8 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/ownerDetails"))
                 .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
+
+
+
+
 }
